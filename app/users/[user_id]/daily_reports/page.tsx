f096c674e80d4b0e6d6fa658ea1c7b8833c6fdcd
yasuda <samonkntd@gmail.com>
2025-07-07 21:13:49 +0900

    fix/deploy: error fix v1

--- app/users/[user_id]/daily_reports/page.tsx
+++ app/users/[user_id]/daily_reports/page.tsx
@@ -2,13 +2,11 @@ import { ReportContent } from "@/components/daily-reports/report-content";
 import Loading from "./loading";
 import { Suspense } from "react";
 
-type Props = {
-  params: {
-    user_id: string;
-  };
-};
-
-export default function DailyReportsPage({ params }: Props) {
+export default async function DailyReportsPage({
+  params,
+}: {
+  params: { user_id: string };
+}) {
   return (
     <Suspense fallback={<Loading />}>
       <ReportContent userId={params.user_id} />
--- app/users/[user_id]/tasks/page.tsx
+++ app/users/[user_id]/tasks/page.tsx
@@ -2,13 +2,13 @@ import { TaskContent } from "@/components/tasks/task-content";
 import Loading from "./loading";
 import { Suspense } from "react";
 
-type Props = {
+export default async function TasksPage({
+  params,
+}: {
   params: {
     user_id: string;
   };
-};
-
-export default function TasksPage({ params }: Props) {
+}) {
   return (
     <Suspense fallback={<Loading />}>
       <TaskContent userId={params.user_id} />
