77a360afb27d2be4ef3debe2b923a07dae5c96b2
yasuda <samonkntd@gmail.com>
2025-07-09 09:49:52 +0900

    fix/lint_deploy: error fix v1

--- app/users/[user_id]/tasks/page.tsx
+++ app/users/[user_id]/tasks/page.tsx
@@ -1,7 +1,6 @@
 import { TaskContent } from "@/components/tasks/task-content";
 import Loading from "./loading";
 import { Suspense } from "react";
-import { Task } from "@/lib/definitions";
 
 export default async function TasksPage({
   params,
